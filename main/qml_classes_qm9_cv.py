1a215c0d36da9ea519081217bdcbeda9a6f32c2b
b3rn4rdm <bernard.mazouin94@gmail.com>
2021-08-19 12:46:49 +0200

    qml with classification for qm9 data

---
+++ main/qml_classes_qm9_cv.py
@@ -0,0 +1,269 @@
+import numpy as np
+import pickle
+import sys
+import os
+
+from qml.kernels import gaussian_kernel, laplacian_kernel
+from qml.kernels import gaussian_kernel_symmetric, laplacian_kernel_symmetric
+from sklearn.model_selection import train_test_split
+
+from qml_krr import *
+
+from uuid import uuid4
+from hashlib import md5
+
+
+def load_data(datadir, dataname, prop):
+    data = np.load(f'{datadir}{dataname}')
+    X = data['X']
+    Y = data[prop]
+    labels = data['labels']
+    U = data['U']
+    R = data['R']
+    return (X, Y, labels, U, R)
+
+
+def group_by_label(a, labels):
+    a_subsets = []
+    u_labels = np.unique(labels)
+    for ul in u_labels:
+        a_tmp = []
+        for ai, l in zip(a, labels):
+            if l == ul:
+                a_tmp.append(ai)
+        a_subsets.append(np.asarray(a_tmp))
+    return (a_subsets, u_labels)
+
+
+def choose_samples(X, G, nsamples, seed_select):
+    np.random.seed(seed_select)
+    indices_tot = np.arange(len(X)).astype(int)
+    np.random.shuffle(indices_tot)
+    indices_samples = indices_tot[:nsamples]
+    X_samples = X[indices_samples]
+    G_samples = G[indices_samples]
+    return (X_samples, G_samples)
+
+
+def get_train_test_splits(class_sizes, labels, max_train, max_train_sizes, seed_test):
+    """
+    args: 
+        class_sizes : list with the sizes (int) of the classes
+        labels : list with labels
+        max_train : largest training set size overall
+        max_train_sizes : largets training set sizes for the respective classes
+        seed_test : seed for the random index generation
+    """
+
+    # identify indices of the test splits within the classes
+    test_ind_subsets = []
+    train_ind_subsets = []
+    for cs, mts in zip(class_sizes, max_train_sizes):
+        tss = cs - mts
+        # print(tss)
+        train_ind, test_ind = train_test_split(np.arange(cs).astype(int),
+                                               test_size=tss, random_state=seed_test)
+        # print(train_ind.shape, test_ind.shape)
+        train_ind_subsets.append(train_ind)
+        test_ind_subsets.append(test_ind)
+
+    # retrace corresponding indices of the original dataset
+    unique_labels = np.sort(np.unique(labels))
+    label_indices = []
+    for ul in unique_labels:
+        label_indices.append(np.where(labels == ul)[0])
+
+    original_test_indices = []
+    original_train_indices = []
+    for lindices, test_ind, train_ind in zip(label_indices, test_ind_subsets, train_ind_subsets):
+        original_test_indices.append(lindices[test_ind])
+        original_train_indices.append(lindices[train_ind])
+
+    all_test_indices = np.concatenate(original_test_indices)
+    all_train_indices = np.concatenate(original_train_indices)
+
+    # print(all_test_indices.shape, all_train_indices.shape)
+
+    assert np.intersect1d(all_train_indices, all_test_indices).shape[0] == 0, 'error in train test splitting!'
+
+    # create indices for the training sets
+    return (all_train_indices, original_test_indices, train_ind_subsets, test_ind_subsets)
+
+
+def do_qml(X_train, X_test, Y_train, Y_test, kernel, sigma, seed_cv, lam, n_folds, data_uuid,
+           **kwargs):
+
+    if kernel == 'gaussian':
+        kernel_train = gaussian_kernel_symmetric(X_train, sigma)
+        kernel_pred = gaussian_kernel(X_test, X_train, sigma)
+    elif kernel == 'laplacian':
+        kernel_train = laplacian_kernel_symmetric(X_train, sigma)
+        kernel_pred = laplacian_kernel(X_test, X_train, sigma)
+
+    print(f'CV for sigma = {sigma}: ')
+
+    res_cv = qml_cross_validate(kernel_train, Y_train, sigma, data_uuid, n_folds, lam, seed_cv)
+
+    mae_list = np.asarray([res_cv[k]['test']['mae'] for k in range(n_folds)])
+    std_list = np.asarray([res_cv[k]['test']['std'] for k in range(n_folds)])
+    med_list = np.asarray([res_cv[k]['test']['med'] for k in range(n_folds)])
+
+    # compute regression coefficients
+    if ('method' in kwargs) & ('cond' in kwargs):
+        alpha = qml_fit(kernel_train, Y_train, lam, method, cond)
+    else:
+        alpha = qml_fit(kernel_train, Y_train, lam)
+        
+    # make predictions
+    Y_pred_train = qml_predict(kernel_train, alpha)
+    Y_pred_test = qml_predict(kernel_pred, alpha)
+    
+    # evaluate results
+    mae_train, std_train, med_train = qml_evaluate(Y_pred_train, Y_train)
+    mae_test, std_test, med_test = qml_evaluate(Y_pred_test, Y_test)
+
+    print(f'Error on test set with sigma = {sigma}', mae_test)
+
+    results = {'train': {'mae': mae_train, 'std': std_train, 'med': med_train,
+                         'ref': Y_train, 'pred': Y_pred_train},
+               'test': {'mae': mae_test, 'std': std_test, 'med': med_test,
+                        'ref': Y_test, 'pred': Y_pred_test},
+               'cv': {'mae': mae_list, 'std': std_list, 'seed': seed_cv}}
+    
+    results['uuid'] = uuid4().hex
+
+    print('\n')
+
+    return results
+
+
+def get_rand_indices(n_samples, n_train, seed_qml):
+
+    indices = np.arange(n_samples).astype(int)
+    np.random.seed(seed_qml)
+    np.random.shuffle(indices)
+
+    return indices[:n_train]
+
+
+def main(datadir, dataname, outputdir, outputname, prop, seed_test, seed_train, seed_cv,
+         ktype, n_fold, lam_exp, comment=''):
+    """
+    args : 
+        datadir
+        dataname
+        outputdir
+        outputname
+        prop
+        seed_test
+        seed_train
+        seed_cv
+        ktype
+        n_fold
+        lam_exp
+        comment
+    """
+    
+    seed_test = int(seed_test)
+    seed_train = int(seed_train)
+    seed_cv = int(seed_cv)
+    n_fold = int(n_fold)
+    lam_exp = int(lam_exp)
+    
+    # load the dataset
+    X, Y, labels, U, R = load_data(datadir, dataname, prop)
+
+    X_subsets, u_labels = group_by_label(X, labels)
+    Y_subsets, u_labels = group_by_label(Y, labels)
+
+    max_train = 6400
+    max_train_sizes = np.asarray([3200, 3200, 1600])
+    class_sizes = np.asarray([sub.shape[0] for sub in X_subsets])
+
+    train_ind, test_ind, train_ind_subsets, test_ind_subsets = get_train_test_splits(class_sizes, labels, max_train, max_train_sizes, seed_test)
+
+    # parameters ofr qml
+    sigmas = np.logspace(7, 11, num=5, base=2, endpoint=True)
+    lam = 10 ** (lam_exp)
+
+    qml_params = {'lam': lam, 'ktype': ktype, 'seed': seed_cv}
+    cv_params = {'n_fold': n_fold}
+
+    subsets = [-1, 0, 1, 2]
+
+    for sub in subsets:
+
+        print(f'Running CV for subset {sub}: ')
+
+        results = {}
+
+        for i, sig in enumerate(sigmas):
+
+            # print(f'\t Compute kernels for sigma = {sig}')
+
+            if sub == -1:
+                np.random.seed(seed_train)
+                rand_indices = np.random.choice(train_ind, max_train, replace=False)
+                X_samples = X[rand_indices]
+                Y_samples = Y[rand_indices]
+                if ktype == 'gaussian':
+                    kernel = gaussian_kernel_symmetric(X_samples, sig)
+                elif ktype == 'laplacian':
+                    kernel = laplacian_kernel_symmetric(X_samples, sig)
+            else:
+                np.random.seed(seed_train)
+                rand_indices = np.random.choice(train_ind_subsets[sub], max_train_sizes[sub],
+                                                replace=False)
+                X_samples = X_subsets[sub][rand_indices]
+                Y_samples = Y_subsets[sub][rand_indices]
+                if ktype == 'gaussian':
+                    kernel = gaussian_kernel_symmetric(X_samples, sig)
+                elif ktype == 'laplacian':
+                    kernel = laplacian_kernel_symmetric(X_samples, sig)
+            
+            qml_params['sigma'] = sig
+
+            # create QML object
+            qml_krr = QML_KRR()
+            qml_krr.add_params( ** qml_params)
+            qml_krr.add_input(X_samples)
+            qml_krr.add_labels(Y_samples)
+            qml_krr.add_kernel(kernel)
+
+            cv_qml_krr = CV_QML_KRR(qml_krr)
+            cv_qml_krr.add_params( ** cv_params)
+            
+            result_cv = cv_qml_krr.run()
+            mae_train_cv, mae_test_cv = cv_qml_krr.evaluate()
+
+            print(f'\tsigma = {sig} : MAE = {mae_test_cv} eV')
+            
+            results[sig] = cv_qml_krr.get_summary()
+
+        results['subset'] = sub
+        results['prop'] = prop
+        results['seed_test'] = seed_test
+        results['seed_train'] = seed_train
+
+        # add comment to results
+        results['comment'] = comment
+
+        uid = uuid4().hex
+        results['uuid'] = uid
+        hash_str = f'qm9 cv {prop} {sub} {R} {ktype} {lam_exp}'
+        res_hash = md5(bytes(hash_str, 'utf-8')).hexdigest()
+        results['R'] = R
+        results['type'] = 'cv'
+        results['dataset'] = 'qm9'
+        results['hash'] = res_hash
+        
+        # save resutls
+        with open(f'{outputdir}{outputname}_{res_hash}.pkl', 'wb') as outf:
+            pickle.dump(results, outf)
+
+    return
+
+
+if __name__ == '__main__':
+
+    main( * sys.argv[1:])
---
+++ main/qml_classes_qm9_cv_loop.py
@@ -0,0 +1,346 @@
+import numpy as np
+import pickle
+import sys
+import os
+
+from qml.kernels import gaussian_kernel_symmetric, laplacian_kernel_symmetric
+
+from qml_krr import *
+
+from qml_classes_qm9_cv import group_by_label, get_train_test_splits
+
+from uuid import uuid4
+from hashlib import md5
+
+
+def load_data(datadir, dataname):
+    data = np.load(f'{datadir}{dataname}')
+    X = data['X']
+    G = data['G']
+    H = data['H']
+    L = data['L']
+    labels = data['labels']
+    U = data['U']
+    R = data['R']
+    return (X, G, H, L, labels, U, R)
+
+
+def run(X, Y, indices, test_ind, qml_krr, n_train, seed_qml, seed_iter, n_iter):
+
+    results = {}
+
+    new_seed = seed_qml
+
+    for i in range(n_iter):
+
+        # print(f'\tStarting iteration {i}...')
+
+        # print(f'\t\tSplitting training from test set...')
+        np.random.seed(new_seed)
+        train_ind = np.random.choice(indices, n_train, replace=False)
+
+        qml_krr.add_train_test_indices(train_ind, test_ind)
+        qml_krr.split_input()
+        qml_krr.split_labels()
+        X_train = X[train_ind]
+        Y_train = Y[train_ind]
+        kernel_train = qml_krr.compute_kernel_train()
+        kernel_pred = qml_krr.compute_kernel_pred()
+
+        # print(f'\t\t...fitting alphas...')
+        alpha = qml_krr.fit()
+
+        # print(f'\t\t...preditions...')
+        Y_pred_train, Y_pred_test = qml_krr.predict()
+        mae_train, mae_test = qml_krr.evaluate()
+        res_summary = qml_krr.get_summary()
+        results[i] = copy(res_summary)
+
+        print(f'\tIteration {i} : MAE = {mae_test} eV')
+
+        # print('\t\t...resetting the seed...')
+        new_seed = int(new_seed / seed_iter)
+        # print('\t\t..done!')
+
+        # print('\t...done!')
+
+    mae_train_av, mae_test_av, std_err_train, std_err_test = evaluate(results, n_iter)
+
+    return get_summary(results, mae_train_av, mae_test_av, std_err_train, std_err_test,
+                       n_iter, seed_iter)
+
+
+def evaluate(results, n_iter):
+
+    mae_train_iter = []
+    mae_test_iter = []
+    
+    for i in range(n_iter):
+
+        mae_train_iter.append(results[i]['mae_train'])
+        mae_test_iter.append(results[i]['mae_test'])
+
+    mae_train_av = np.mean(mae_train_iter)
+    mae_test_av = np.mean(mae_test_iter)
+
+    std_err_train = np.std(mae_train_iter)
+    std_err_test = np.std(mae_test_iter)
+
+    return (mae_train_av, mae_test_av, std_err_train, std_err_test)
+
+
+def get_summary(results, mae_train_av, mae_test_av, std_err_train, std_err_test,
+                n_iter, seed_iter):
+
+    res_summary = {'results': results,
+                   'mae_train_av': mae_train_av, 'mae_test_av': mae_test_av,
+                   'std_err_train': std_err_train, 'std_err_test': std_err_test,
+                   'n_iter': n_iter, 'seed_iter': seed_iter}
+
+    return res_summary
+
+
+def run_splits(X, Y, indices, test_ind_subsets, qml_krr, n_train, seed_qml, seed_iter, n_iter,
+               subsets_test):
+    """
+    same as above but runs over multiple test set splits
+    """
+
+    results_iter = {}
+
+    new_seed = seed_qml
+
+    for i in range(n_iter):
+
+        # print(f'\tStarting iteration {i}...')
+
+        # print(f'\t\tSplitting training from test set...')
+        np.random.seed(new_seed)
+        train_ind = np.random.choice(indices, n_train, replace=False)
+
+        X_train = X[train_ind]
+        Y_train = Y[train_ind]
+
+        if qml_krr.ktype == 'laplacian':
+            kernel_train = laplacian_kernel_symmetric(X_train, qml_krr.sigma)
+        elif qml_krr.ktype == 'gaussian':
+            kernel_train = gaussian_kernel_symmetric(X_train, qml_krr.sigma)
+
+        alpha = cho_solve(kernel_train + qml_krr.lam * np.eye(n_train), Y_train)
+
+        results_iter[i] = {}
+        
+        for j, test_ind in enumerate(test_ind_subsets):
+
+            # split again, the train indices are the same but the test indices have changed
+            X_test = X[test_ind]
+            Y_test = Y[test_ind]
+            if qml_krr.ktype == 'laplacian':
+                kernel_pred = laplacian_kernel(X_test, X_train, qml_krr.sigma)
+            elif qml_krr.ktype == 'gaussian':
+                kernel_pred = gaussian_kernel(X_test, X_train, qml_krr.sigma)
+
+            Y_pred_train = np.dot(kernel_train, alpha)
+            Y_pred_test = np.dot(kernel_pred, alpha)
+            
+            mse_train, mse_test, mae_train, mae_test = evaluate_splits(Y_train, Y_test,
+                                                                       Y_pred_train, Y_pred_test)
+            res_summary = get_summary_splits(mse_train, mse_test, mae_train, mae_test,
+                                             Y_train, Y_test, Y_pred_train, Y_pred_test,
+                                             train_ind, test_ind, n_train, new_seed)
+            
+            results_iter[i][j] = copy(res_summary)
+
+            print(f'\tIteration {i}, subset {j} : MAE = {mae_test} eV')
+
+        # print('\t\t...resetting the seed...')
+        new_seed = int(new_seed / seed_iter)
+        # print('\t\t..done!')
+
+        # print('\t...done!')
+
+    results_re = rearrange_results(results_iter, n_iter, subsets_test)
+    results_sub = {}
+    for sub_test, res_sub in results_re.items():
+        mae_train_av, mae_test_av, std_err_train, std_err_test = evaluate_iter(res_sub, n_iter)
+        results_sub[sub_test] = get_summary_iter(res_sub, mae_train_av, mae_test_av,
+                                                 std_err_train, std_err_test, n_iter, seed_iter)
+
+    return results_sub
+
+
+def rearrange_results(results_iter, n_iter, subsets_test):
+
+    results_new = {}
+    for sub in subsets_test:
+        results_new[sub] = {}
+        for i in range(n_iter):
+            results_new[sub][i] = results_iter[i][sub]
+
+    return results_new    
+
+
+def evaluate_splits(Y_train, Y_test, Y_pred_train, Y_pred_test):
+
+    mse_train = np.mean(Y_train - Y_pred_train)
+    mse_test = np.mean(Y_test - Y_pred_test)
+
+    mae_train = np.mean(np.abs(Y_train - Y_pred_train))
+    mae_test = np.mean(np.abs(Y_test - Y_pred_test))
+
+    return (mse_train, mse_test, mae_train, mae_test)
+
+
+def evaluate_iter(results, n_iter):
+
+    mae_train_iter = []
+    mae_test_iter = []
+    for i in range(n_iter):
+        mae_train_iter.append(results[i]['mae_train'])
+        mae_test_iter.append(results[i]['mae_test'])
+
+    mae_train_av = np.mean(mae_train_iter)
+    mae_test_av = np.mean(mae_test_iter)
+
+    std_err_train = np.std(mae_train_iter)
+    std_err_test = np.std(mae_test_iter)
+
+    return (mae_train_av, mae_test_av, std_err_train, std_err_test)
+
+
+def get_summary_splits(mse_train, mse_test, mae_train, mae_test, Y_train, Y_test,
+                       Y_pred_train, Y_pred_test, train_ind, test_ind, n_train, seed):
+
+    res_summary = {'mse_train': mse_train, 'mse_test': mse_test,
+                   'mae_train': mae_train, 'mae_test': mae_test,
+                   'Y_train': Y_train, 'Y_test': Y_test,
+                   'Y_pred_train': Y_pred_train, 'Y_pred_test': Y_pred_test,
+                   'train_ind': train_ind, 'test_ind': test_ind,
+                   'n_train': n_train, 'seed': seed}
+
+    return res_summary
+
+
+def get_summary_iter(results, mae_train_av, mae_test_av, std_err_train, std_err_test,
+                     n_iter, seed_iter):
+
+    results_sum = {'results': results,
+                   'mae_train_av': mae_train_av, 'mae_test_av': mae_test_av,
+                   'std_err_train': std_err_train, 'std_err_test': std_err_test,
+                   'n_iter': n_iter, 'seed_iter': seed_iter}
+    
+    return results_sum
+
+
+def main(datadir, dataname, outputdir, outputname, prop, seed_test, seed_qml, seed_iter,
+         ktype, n_iter, lam_exp, n_train, comment=''):
+
+    seed_test = int(seed_test)
+    seed_qml = int(seed_qml)
+    seed_iter = int(seed_iter)
+    n_iter = int(n_iter)
+    # sigma = float(sigma)
+    n_train = int(n_train)
+    lam_exp = int(lam_exp)
+
+    # load the dataset
+    X, G, H, L, labels, U, R = load_data(datadir, dataname)
+
+    if prop == 'G':
+        Y = G
+    elif prop == 'H':
+        Y = H
+    elif prop == 'L':
+        Y = L
+
+    X_subsets, u_labels = group_by_label(X, labels)
+    Y_subsets, u_labels = group_by_label(Y, labels)
+    # G_subsets, u_labels = group_by_label(G, labels)
+    # H_subsets, u_labels = group_by_label(H, labels)
+    # L_subsets, u_labels = group_by_label(L, labels)
+
+    max_train = 64000
+    max_train_sizes = np.asarray([32000, 32000, 16000])
+    class_sizes = np.asarray([sub.shape[0] for sub in X_subsets])
+
+    train_ind, test_ind, train_ind_subsets, test_ind_subsets = get_train_test_splits(class_sizes, labels, max_train, max_train_sizes, seed_test)
+
+    lam = 10 ** (lam_exp)
+    params = {'lam': lam, 'ktype': ktype}
+    qml_krr = QML_KRR()
+    qml_krr.add_params( ** params)
+
+    if n_train <= 16000:
+        subsets = [-1, 0, 1, 2]
+    elif n_train == 32000:
+        subsets = [-1, 0, 1]
+    elif n_train == 64000:
+        subsets = [-1]
+
+    subsets_test = [0, 1, 2]
+
+    print(f'n_train = {n_train} | subsets = {subsets}')
+
+    sigmas = {-1: 256., 0: 256., 1: 256., 2: 256.}
+
+    for sub in subsets:
+
+        print(f'Calculating for subset {sub}.')
+    
+        if (sub == -1):
+            X_samples = X
+            Y_samples = Y
+            qml_krr.set_sigma(sigmas[sub])
+            qml_krr.add_input(X_samples)
+            qml_krr.add_labels(Y_samples)
+            results = {}
+            # for sub_test in subsets_test:
+            #     results[sub_test] = run(X_samples, Y_samples, train_ind, test_ind[sub_test], qml_krr,
+            #                             n_train, seed_qml, seed_iter, n_iter)
+            results = run_splits(X_samples, Y_samples, train_ind, test_ind, qml_krr,
+                                 n_train, seed_qml, seed_iter, n_iter, subsets_test)
+        else:
+            X_samples = X_subsets[sub]
+            Y_samples = Y_subsets[sub]
+            qml_krr.set_sigma(sigmas[sub])
+            qml_krr.add_input(X_samples)
+            qml_krr.add_labels(Y_samples)
+            results = run(X_samples, Y_samples, train_ind_subsets[sub], test_ind_subsets[sub],
+                          qml_krr, n_train, seed_qml, seed_iter, n_iter)
+    
+        # add ktype to results
+        results['ktype'] = ktype
+
+        results['subset'] = sub
+        results['prop'] = prop
+
+        # add comment to results
+        # results['comment'] = comment
+
+        # save resutls
+        uid = uuid4().hex
+        results['uuid'] = uid
+
+        sig = sigmas[sub]
+        results['sigma'] = sig
+        results['seed_test'] = seed_test
+        results['seed_qml'] = seed_qml
+        results['seed_iter'] = seed_iter
+        results['type'] = 'loop'
+        results['dataset'] = 'qm9'
+
+        hash_str = f'qm9 loop {prop} {sub} {R} {ktype} {sig} {lam_exp} {n_train}'
+        res_hash = md5(bytes(hash_str, 'utf-8')).hexdigest()
+        results['hash'] = res_hash
+
+        with open(f'{outputdir}{outputname}_{res_hash}.pkl', 'wb') as outf:
+            pickle.dump(results, outf)
+
+    return
+
+
+if __name__ == '__main__':
+
+    main( * sys.argv[1:])
+
+
